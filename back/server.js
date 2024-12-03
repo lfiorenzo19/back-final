db9cd6ba2c58fe61d1e68ff5c4ed315e3cc40e53
lfiorenzo19 <leofiorenzo9@gmail.com>
2024-12-03 17:30:50 -0300

    puerto

--- back/server.js
+++ back/server.js
@@ -42,7 +42,7 @@ mongoose.connect(process.env.MONGODB_URI)
   }).catch(err => console.error('Error al conectar a MongoDB:', err));
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4173;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 }); 
