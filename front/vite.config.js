137a042dcc14c09ceef0e900e96b923163d0196c
lfiorenzo19 <leofiorenzo9@gmail.com>
2024-12-03 19:27:16 -0300

    cambio3

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 3000 // Puedes usar el puerto 3000 para el desarrollo local
-  },
-  build: {
-    outDir: 'dist', // Asegúrate de que el directorio de salida sea 'dist'
+  server:{
+    host:"0.0.0.0",
+    port:10000
   },
   plugins: [react()],
 });
